e3470e4867bc2411de048ca34ad119f1305e9560
Marcos Contente <marcosvgcontente@correio.itau-unibanco.com.br>
2018-10-19 18:12:58 -0300

    Start the project

---
+++ Jokenpo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jokenpo
+//
+//  Created by Marcos V G Contente on 19/10/18.
+//  Copyright © 2018 marcoscontente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
